f6b948584a05c81c387c090e5c9c70b192dd167a
developeranalyser <151624389+developeranalyser@users.noreply.github.com>
2024-04-27 09:04:56 +0330

    torch.cuda.empty_cache()

--- src/seamless_communication/cli/m4t/finetune/trainer.py
+++ src/seamless_communication/cli/m4t/finetune/trainer.py
@@ -437,7 +437,6 @@ class UnitYFinetune:
                             "Early termination, as eval loss did not improve "
                             f"over last {no_improve_steps} updates"
                         )
-                        del no_improve_steps
                         break
                 self.update_idx += 1
             self.epoch_idx += 1
