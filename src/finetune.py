0b1deb07cc5d55e1d86a2ac8aa94d40e486ed5b1
developeranalyser <151624389+developeranalyser@users.noreply.github.com>
2024-05-07 10:27:13 +0330

    reverse original save

--- src/finetune.py
+++ src/finetune.py
@@ -167,6 +167,7 @@ def main() -> None:
             and model.t2u_model is not None):
         model.t2u_model = None
 
+    model.input_modality = 'text'
     #### for TEXT_TO_SPEECH
     # بررسی وجود pth مربوطه
     # if str(args.t2u_model_path) == 'default':
